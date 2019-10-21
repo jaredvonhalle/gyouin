7653cf06fb3e4cbd6bfe319003c3f3f8b900e298
Jared von Halle <C-Jared.VonHalle@charter.com>
2019-10-21 16:09:58 -0600

    half way through save

--- src/OverviewBets.js
+++ src/OverviewBets.js
@@ -7,18 +7,20 @@ class OverviewBets extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {bets:[]};
+    this.state = {
+      bets:[],
+      changedInd:[]
+    };
     this.myRef = React.createRef();
-    this.challengerClick = this.challengerClick.bind(this);
-  }
-
-  challengerClick(e) {
-    console.log(e);
+    this.renderEditable = this.renderEditable.bind(this);
+    this.saveRow = this.saveRow.bind(this);
   }
 
   componentDidMount() {
+    const url = '/api/Bets';
+
     fetch(
-      '/api/Bets'
+      url
     )
     .then(response => {
       return response.json();
@@ -29,39 +31,127 @@ class OverviewBets extends Component {
     
   }
 
+  saveRow(cellInfo) {
+    console.log(cellInfo);
+    this.postData(cellInfo);
+  }
+
+  postData(cellInfo) {
+    let data = this.state.bets[cellInfo.index]
+    const url = '/api/Bets/' + data.id;
+    // The data we are going to send in our request
+    
+    let jsonData = JSON.stringify(data)
+    // The parameters we are gonna pass to the fetch function
+    let fetchData = { 
+        method: 'PUT', 
+        body: jsonData,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+    }
+    fetch(url, fetchData)
+    .then(response => {
+        console.log(response)
+    });
+  }
+
+  isChanged(cellInfo) {
+    var ind = this.state.changedInd.includes(456);
+    return ind;
+  }
+
+  renderEditable(cellInfo) {
+    return (
+      <div
+        contentEditable
+        suppressContentEditableWarning
+        onBlur={e => {
+          const data = [...this.state.bets];
+          data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
+          this.setState({ data });
+          this.setState({changedInd:[456]})
+        }}
+        dangerouslySetInnerHTML={{
+          __html: this.state.bets[cellInfo.index][cellInfo.column.id]
+        }}
+      />
+    );
+  }
+
 	render() {
 
     const columns = [{
-      Header: 'challenger',
-      accessor: 'challenger'
+      Header: 'Challenger',
+      accessor: 'challenger',
+      Cell: this.renderEditable
     },{
-      Header: 'odds',
-      accessor: 'odds'
+      Header: 'Odds',
+      accessor: 'odds',
+      Cell: this.renderEditable
     },{
-      Header: 'description',
-      accessor: 'description'
+      Header: 'Description',
+      accessor: 'description',
+      Cell: this.renderEditable
     },{
-      Header: 'accepter',
-      accessor: 'accepter'
+      Header: 'Accepter',
+      accessor: 'accepter',
+      Cell: this.renderEditable
     },{
-      Header: 'amount',
-      accessor: 'amount'
+      Header: 'Base Amount',
+      accessor: 'amount',
+      Cell: this.renderEditable
     },{
-      Header: 'createDate',
+      Header: 'Create Date',
       accessor: 'createDate'
     },{
-      Header: 'endDate',
-      accessor: 'endDate'
+      Header: 'End Date',
+      accessor: 'endDate',
+      Cell: this.renderEditable
     },{
-      Header: 'status',
+      Header: 'Status',
       accessor: 'status'
-    }]
+    },{
+      Header: 'Save',
+      Cell: props => {
+        return(
+          <button className={(this.isChanged(props) ? 'editable' : 'not')} onClick={() => this.saveRow(props)}>
+            Save
+          </button>
+        )
+      },
+      filterable: false,
+      sortable: false,
+      width:100,
+      maxWidth:100,
+      minWidth:100
+    },{
+      Header: 'Delete',
+      Cell: props => {
+        return(
+          <button>Delete</button>
+        )
+      },
+      filterable: false,
+      sortable: false,
+      width:100,
+      maxWidth:100,
+      minWidth:100
+    }
+  ]
     return (
       <div className="overview-bets container-fluid">
         <ReactTable
           data={this.state.bets}
           columns={columns}
           defaultPageSize = {10}
+          filterable
+          defaultSorted={[
+            {
+              id: "endDate"
+            }
+          ]}
         />
       </div>
     );
